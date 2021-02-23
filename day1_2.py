a9514a7bc56df3aebe75806bd225854e944a73f0
buelmanager@gmail.com <buelmanager@gmail.com>
2021-02-23 09:57:03 +0900

    # 연산자

--- day1_2.py
+++ day1_2.py
@@ -1,1 +1,16 @@
 #연산자 
+
+print (1+1) #2
+print (3-2) #1
+print (2*3) #6
+print (6/2) #3
+
+print (2**3) # 2^3 8
+print (5%3) # 나머지 구하기 2
+print (10%3)    # 1
+print (5//3)    # 몫 구하기 1
+print (10//3)   # 3
+
+print ( 10>3 )  # True
+
+
